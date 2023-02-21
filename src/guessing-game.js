66c034cd7188d93428a7066c5c3d802a017a1f64
irvelds <irvelds@mail.ru>
2023-02-21 16:19:23 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.mid = 0;
+        this.min = 0;
+        this.max = 0;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        this.mid = Math.round((this.min + this.max) / 2);
+        return this.mid;
     }
-
     lower() {
-
+        this.max = this.mid;
     }
-
     greater() {
-
+        this.min = this.mid;
     }
 }
 
